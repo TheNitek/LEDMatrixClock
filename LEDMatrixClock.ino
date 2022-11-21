79fcc2d01e027a2b883ff9ecbbe412afa3ba6448
Nitek <nitek@blickt.es>
2022-11-21 22:44:15 +0100

    Show countdown

--- LEDMatrixClock.ino
+++ LEDMatrixClock.ino
@@ -4,27 +4,31 @@
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
 WiFiUDP ntpUDP;
 // You can specify the time server pool and the offset (in seconds, can be
 // changed later with setTimeOffset() ). Additionaly you can specify the
 // update interval (in milliseconds, can be changed using setUpdateInterval() ).
-NTPClient timeClient(ntpUDP, "fritz.box", 1*3600, 60000);
+NTPClient timeClient(ntpUDP, "de.pool.ntp.org", 1*3600, 60000);
 
 
 MD_Parola P = MD_Parola(MD_MAX72XX::FC16_HW, D2, 4);
 
-uint8_t scrollSpeed = 25;    // default frame delay value
+uint8_t scrollSpeed = 0;    // default frame delay value
 //textEffect_t scrollEffect = PA_SCROLL_LEFT;
 textEffect_t scrollEffect = PA_PRINT;
 textPosition_t scrollAlign = PA_CENTER;
 uint16_t scrollPause = 2000; // in milliseconds
-#define BUF_SIZE 9
 
 WiFiManager wifiManager;
 
+const unsigned long targetTime = 1670198400;
+unsigned long lastDiff = 0;
+
+#define BUF_SIZE 11
+
+char buffer[BUF_SIZE] = "";
+    
 void setup() {
   // Seems to make flashing more reliable
   delay(100);
@@ -32,6 +36,9 @@ void setup() {
   P.begin();
 
   Serial.begin(115200);
+  Serial.println();
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
   Serial.println();
   Serial.println("Starting ...");
   P.setIntensity(1);
@@ -41,26 +48,24 @@ void setup() {
   wifiManager.autoConnect("TimerSetup", "timertimer");
   timeClient.begin();
 
-  P.displayAnimate();
-
-  ArduinoOTA.setHostname("ledclock");
-  ArduinoOTA.begin();
   Serial.println("Done");
 }
 
 void loop() {
   timeClient.update();
-  ArduinoOTA.handle();
 
-  int hours = timeClient.getHours();
-  int minutes = timeClient.getMinutes();
-  int seconds = timeClient.getSeconds();
+  unsigned long timeDiff = (targetTime - timeClient.getEpochTime());
+  if(timeDiff > 1000000) {
+    timeDiff = timeDiff/60;
+  }
 
-  char buffer[BUF_SIZE];
+  if(timeDiff != lastDiff) {
+    sprintf(buffer, "%lu", timeDiff);
+    Serial.println(buffer);
+    P.displayText(buffer, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+    lastDiff = timeDiff;
+  }
 
-  sprintf(buffer, "%02d:%02d", hours, minutes, seconds);
-
-  P.displayText(buffer, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
   if(P.displayAnimate()) {
     P.displayReset();
   }
